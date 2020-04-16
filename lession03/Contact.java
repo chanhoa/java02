3b8ce4269a0dd77c6112c3000a71278f506be3eb
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-04-16 21:08:09 +0700

    chanhoa

---
+++ lession03/Contact.java
@@ -0,0 +1,10 @@
+package lession3;
+public class Contact {
+    String name;
+    String phone;
+
+    void viewInfo() {
+        System.out.println("Name  : "+name);
+        System.out.println("Phone : "+phone);
+    }
+}
