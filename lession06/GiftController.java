e9519333ff478cadfda85bd11537edfca1d4c4a5
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-04-27 18:50:00 +0700

    chanhoa

---
+++ lession06/GiftController.java
@@ -0,0 +1,13 @@
+package lession6;
+
+public class GiftController {
+    String sqlInsert,sqlDelete;
+    public String insertBook(Gift books){
+        sqlInsert = "Insert into books(id,author,price,qty) values(" + books.getId() + ",'" + books.getName() + "'," + books.getPrice() + "," + books.getQty() + ")";
+        return sqlInsert;
+    }
+    public String deleteBook(int idDelete){
+        sqlDelete = "Delete from books where id = " + idDelete;
+        return sqlDelete;
+    }
+}
